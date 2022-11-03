62480733af30a45348ace41e903f8d9d8fda4868
heheer <1239331448@qq.com>
2022-11-03 18:10:28 +0800

    add react_advanced

---
+++ React_Advanced/reusability/src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Favorite from "./Favorite"
+import Menu from "./Menu"
+
+function App() {
+    return (
+        <div>
+            <Menu />
+            <hr />
+            <Favorite />
+        </div>
+    )
+}
+
+export default App
---
+++ React_Advanced/reusability/src/Example.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Example(props) {
+    return (
+        <div>
+            {props.render("Bob")}
+        </div>
+    )
+}
+
+export default Example
