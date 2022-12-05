1124a930300d4bf6cf4b2da2ab967e87379c5e86
heheer <1239331448@qq.com>
2022-12-05 22:37:46 +0800

    js_deep_dive

---
+++ JS_Deep_Dive/Variables&String/index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var message = "hello";
+console.log("hello world");
+
+// 1) sloppy "normal" mode - default in scripts
+// 2) strict mode - throws more errors, prevents pitfalls of the language
+
+// hoisting
+
+console.log(age);
+var age = 26;
