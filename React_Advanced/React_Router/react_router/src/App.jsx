e30cb0c52d6d97c4a5e7c4cdba845ed69ec86a0a
heheer <1239331448@qq.com>
2023-03-21 12:37:59 +0800

    react_router

---
+++ React_Advanced/React_Router/react_router/src/App.jsx
@@ -0,0 +1,23 @@
+import { Link, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home"
+import Books  from "./pages/Books"
+
+function App() {
+  return (
+    <>
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/books">Books</Link></li>
+      </ul>
+    </nav>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/books" element={<Books />} />
+      {/* <Route /> */}
+    </Routes>
+    </>
+  ) 
+}
+
+export default App;
